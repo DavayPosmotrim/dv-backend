3abb77778122f98e5c8cf8810ec72c7486d50201
Полина Грунина <linaborisovsky@yandex.ru>
2024-05-06 22:22:15 +0300

    DP12: первоначальные представления жанров,сессий фильмов

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,15 @@
+from collections import Counter
+from itertools import chain
+
 from rest_framework import serializers
 
-from movies.models import Genre
+from movies.models import Genre, Movie
 from custom_sessions.models import CustomSession
 
 
 class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор жанра."""
+
     class Meta:
         model = Genre
         fields = [
@@ -14,6 +19,10 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class CustomSessionSerializer(serializers.ModelSerializer):
+    """Сериализатор сеанса/комнаты."""
+
+    matched_movies = serializers.SerializerMethodField()
+
     class Meta:
         model = CustomSession
         fields = [
@@ -21,5 +30,36 @@ class CustomSessionSerializer(serializers.ModelSerializer):
             'users',
             'movies',
             'date',
-            'status'
+            'status',
+            'matched_movies',
         ]
+
+    def get_matched_movies(self, obj):
+        """Принимает объект сессии, считает голоса пользователей
+        за фильм и выдает совпадения (сохраняет в мэтчи),
+        если голосов больше 1."""
+        movies = obj.movies.all()
+        user_movie_votes = chain.from_iterable(
+            movies.values_list('users', flat=True)
+        )
+        movie_vote_counts = Counter(user_movie_votes)
+        matched_movie_ids = [
+            movie.id for movie in movies
+            if movie_vote_counts[movie.id] > 1
+        ]
+        matched_movies = movies.filter(id__in=matched_movie_ids)
+        return MovieSerializer(matched_movies, many=True).data
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    """Сериализатор фильма/списка фильмов."""
+
+    genre = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=Genre.objects.all()
+    )
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'name', 'genre']
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,50 @@
 from rest_framework import generics
 
-from movies.models import Genre
+from movies.models import Genre, Movie
 from custom_sessions.models import CustomSession
-from .serializers import GenreSerializer, CustomSessionSerializer
+from .serializers import (GenreSerializer, CustomSessionSerializer,
+                          MovieSerializer)
 
 
 class GenreListView(generics.ListAPIView):
+    """Представление списка жанров."""
+
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class CustomSessionCreateView(generics.CreateAPIView):
+    """Создание пользовательского сеанса."""
+
     queryset = CustomSession.objects.all()
     serializer_class = CustomSessionSerializer
 
 
 class UserSessionListView(generics.ListAPIView):
+    """Представление списка сеансов пользователя."""
+
     serializer_class = CustomSessionSerializer
 
     def get_queryset(self):
         user_id = self.kwargs['user_id']
         return CustomSession.objects.filter(users__id=user_id)
+
+
+class MovieListView(generics.ListAPIView):
+    """Представление списка фильмов."""
+
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MatchListView(generics.ListAPIView):
+    """Представление списка избранных фильмов (совпадений)."""
+
+    serializer_class = MovieSerializer
+
+    def get_queryset(self):
+        session_id = self.request.session.get('session_id')
+        if session_id:
+            session = CustomSession.objects.get(id=session_id)
+            return session.movies.filter(matched=True)
+        return Movie.objects.none()
