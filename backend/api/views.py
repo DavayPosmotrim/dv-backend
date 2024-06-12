7cabbfe92bb2d8e0e652dc9c9ec43a7a8d7a6dab
Полина Грунина <linaborisovsky@yandex.ru>
2024-06-12 22:53:15 +0300

    fix imports views

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,12 +9,13 @@ from rest_framework.views import APIView
 from custom_sessions.models import CustomSession
 from movies.models import Genre, Movie
 from services.schemas import (
-    user_schema, user_session_list_schema)
+    user_schema)
 from users.models import User
 
-from .serializers import (
-                          CustomSessionCreateSerializer, CustomUserSerializer,
-                          GenreSerializer, MovieSerializer,
+from .serializers import (CustomSessionCreateSerializer,
+                          CustomUserSerializer,
+                          GenreSerializer,
+                          MovieSerializer,
                           )
 
 
