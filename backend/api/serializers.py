a51e3762a9672b9d0119263ef3ddfed3e4b56ebe
LynnG3 <LynnG3@users.noreply.github.com>
2024-07-26 15:08:25 +0000

    change CustomSessionCreateSerializer, add fields in kinopoisk_movies.get_movies

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,16 +1,12 @@
 import logging
 
 import requests.exceptions
-
+from custom_sessions.models import CustomSession, CustomSessionMovieVote
 from django.db import IntegrityError
+from movies.models import Collection, Genre, Movie
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
-from custom_sessions.models import CustomSession, CustomSessionMovieVote
-from movies.models import Collection, Genre, Movie
-from services.kinopoisk.kinopoisk_service import (
-    KinopoiskMovies
-)
+from services.kinopoisk.kinopoisk_service import KinopoiskMovies
 from services.validators import validate_name
 from users.models import User
 
